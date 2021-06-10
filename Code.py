7333c856484e2de70398040e647f30f516087c36
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-06-10 14:41:35 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -6,21 +6,23 @@ import time
 WIDTH = 800
 HEIGHT = 600
 
-num_bars = 10
-bar_width = 20
+num_bars = 20
+bar_width = 15
 space = 5
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
-screen.fill((255,255,255))
-#pygame.display.update()
+screen.fill((0,0,0))
+
+def button(msg,x,y,w,h,ic,ac):
+    
 
 def drawBar(x,height):
-    pygame.draw.rect(screen,(0,0,0), (x,400,bar_width,height),0)
+    pygame.draw.rect(screen,(255,255,255), (x,200,bar_width,height),0)
 
 for i in range(num_bars):
-    height = random.randint(-100,10 )
-    x = (i*bar_width) + (i*space)
+    height = random.randint(20,200 )
+    x = (i*bar_width) + (i*space) + (WIDTH -(num_bars + bar_width + num_bars + space))/3.5
     drawBar(x, height)  
     
 while True:
